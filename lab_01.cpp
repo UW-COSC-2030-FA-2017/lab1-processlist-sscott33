73dae0a3af4e4132e527845ebc283bff090facdc
sscott33 <sscott33@uwyo.edu>
2017-09-17 11:44:50 -0600

    cleaned up the comments in lab_01.cpp

--- lab_01.cpp
+++ lab_01.cpp
@@ -1,9 +1,8 @@
 // lab_01.cpp : Defines the entry point for the console application.
 //
 
-//#include "stdafx.h"
 
-//#include <cstring>;
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,12 +10,17 @@ using namespace std;
 
 
 void skipWhitespace(ifstream &inputFile);
-		// function to skip whitespaces in an attempt to fix errors with whitespaces at the end of a file
+		// function to skip whitespaces
+		// prevents the nested loop (below) from attempting to read in whitespace at the end of a file
+		// (w/o this the program could duplicate the last number in the file)
 
 
 int main()
 {
 	char yesno = 'n';
+
+	// the following loop serves to prevent the immediate closure of the program upon completion
+	// it also allows for the reuse of the program
 	do {
 		// ask the user for the file name
 		cout << "Please enter the name of the file: ";
@@ -27,43 +31,33 @@ int main()
 
 
 		// find the file and gather stats
-		// report error if the file cannot be accessed/located
 
 		// create an object to read text from the file
 		ifstream inputFile;
 
-
-		// create 2 arrays to store values
-		double firstTwo[2], lastTwo[2];
-
 		// open the file
 		inputFile.open(filename);
 
-		// if (!inputFile.is_open())
-		// 	cout << "\nopen!";
-		// boolean flag to check if there are at least four #s in the file
-		//bool atLeastFourInts = false;
+		// create 2 arrays to store the first and last two values
+		double firstTwo[2], lastTwo[2];
+
 
 		// create a counter to determine how many numbers are in the file
+		// (controls the format of the output)
 		int counter = 0;
 
 		while (!inputFile.eof() && !inputFile.fail()) {
 			// step through file and store the numbers accordingly
-			// if (inputFile.fail())
-			// 	break;
 
-//system("pause");
 			if (counter < 2) {
 				skipWhitespace(inputFile);
 				if (inputFile.eof()) break;
 				inputFile >> firstTwo[counter];
-				//cout << firstTwo[counter];
 			}
 			else if (counter == 2) {
 				skipWhitespace(inputFile);
 				if (inputFile.eof()) break;
 				inputFile >> lastTwo[0];
-				//cout << lastTwo[0];
 			}
 			else {
 				skipWhitespace(inputFile);
@@ -71,20 +65,16 @@ int main()
 				double temp = lastTwo[0];
 				lastTwo[1] = temp;
 				inputFile >> lastTwo[0];
-				//cout << lastTwo[0];
-				//cout << lastTwo[1];
 			}
 
-			// the last two #s are stored in the order of second to last: [1] and last: [0]
+			/// the last two #s are stored in the order of second to last (index of [1]) and then last (index of [0])
 
+			// increment counter after each number has been read
 			counter++;
-
-			// set flag
-			//atLeastFourInts = (counter >= 4);
 
 		}
 
-		//system("pause");
+		// format output based on the quantity of numbers in the file (passed via counter)
 
 		switch (counter)
 		{
@@ -107,25 +97,22 @@ int main()
 
 		}
 
+		// close the file stream
 		inputFile.close();
 
-		//system("pause");
 			// ask the user to reuse the program
 			cout << "\n\nWould you like to use this program again? (y/n) ";
 			cin >> yesno;
-			cout << endl;
+			cout << endl;		// add some space to increase readability if reused
 
 	} while (yesno == 'y' || yesno == 'Y');
-
-
-
-
 
     return 0;
 }
 
 void skipWhitespace(ifstream &inputFile) {
 		while (inputFile.peek() == ' ' || inputFile.peek() == '\n') {
+				// loop through whitespaces in the file until there are no more of them
 				inputFile.get();
 		}
 }
